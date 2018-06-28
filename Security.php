5d01f82320809f49175b5876c7fb53373cc2a755
Hαx0rℓiв - Dєvєℓσρєr <andretutors0@gmail.com>
2018-06-28 13:55:45 -0300

    Update and rename Filter.php to Security.php

--- Security.php
+++ Security.php
@@ -1,20 +1,21 @@
 <?php
 
 /**
- * @package Class Filter
+ * @package Class\Security
  * @author Hax0rlib | IRDeNial
  * @version 0.1.5
  */
 
-    class Filter
+    class Security
     {
 
         /**
-         * Função para validar ataques XSS.
+         * Função para filtrar ataques XSS.
          * 
-         * @param $val
+         * @param string $val
          * @return string
-         */
+         *
+         **/
         public static function XSSFilter(&$val) : string
         {
             if (is_string($val)) {
@@ -35,9 +36,10 @@
         /**
          * Função para validar Email.
          *
-         * @param $val
+         * @param string $val
          * @return bool
-         */
+         *
+         **/
         public static function EmailValidate(string $val) : bool
         {
             return (bool)filter_var($val, FILTER_VALIDATE_EMAIL);
@@ -46,11 +48,12 @@
         /**
          * Função para validar endereço IP.
          *
-         * @param $val
-         * @param $flags
+         * @param int $val
+         * @param int $flags
          * @return bool
-         */
-        public static function IPValidate($val, $flags) : bool
+         *
+         **/
+        public static function IPValidate(int $val, int $flags) : bool
         {
             $flgs = 0;
             switch ($flags) {
@@ -68,6 +71,29 @@
                     break;
             }
             return (bool)filter_var($val, FILTER_VALIDATE_IP, $flgs);
+        }
+
+        /**
+        * Função para setar permissões em diretórios
+        *
+        * @param string $pathName
+        * @param int $perm
+        *
+        **/
+        public static function setDirPerm(string $folder, int $perm)
+        {
+            return (!is_dir($folder) ? mkdir($folder, $perm) : rmdir($folder));
+        }
+
+        /**
+        * A função escape acrescenta barra para evitar a execução de comandos
+        * 
+        * @param string $val
+        *
+        **/
+        public static function CMDEscape(string $val)
+        {
+            return escapeshellcmd($val);
         }
 
 }
